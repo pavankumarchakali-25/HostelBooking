2383a8f8dea6c120f137785a14803b4cd8d70b70
pavankumarchakali-25 <c.pavan9949@gmail.com>
2025-09-12 22:22:33 +0530

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,24 @@
+// Import Firebase SDKs
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-analytics.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+// Your Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyAARKrOGmvkWCcbbgs9uqV4GQXkCEo6d9c",
+  authDomain: "hostel-room-booking-site.firebaseapp.com",
+  projectId: "hostel-room-booking-site",
+  storageBucket: "hostel-room-booking-site.firebasestorage.app",
+  messagingSenderId: "105322833808",
+  appId: "1:105322833808:web:5b0a7d63677f8f33a8b515",
+  measurementId: "G-XE4BFB7RLK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Export services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
