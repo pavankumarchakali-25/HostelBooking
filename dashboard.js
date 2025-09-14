11a50a96efb5cf7df5aea837ec1f16d8a99a3858
pavankumarchakali-25 <c.pavan9949@gmail.com>
2025-09-14 16:29:58 +0530

    dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -13,10 +13,19 @@ auth.onAuthStateChanged(async (user) => {
   const container = document.getElementById("booking-list");
   snapshot.forEach((doc) => {
     const booking = doc.data();
+    if(booking==null) {
     const div = document.createElement("div");
     div.className = "booking-card";
+      div.innerHTML = ' <p>No Bookings</p>;
+        container.appendChild(div);
+    } else{
+      const div = document.createElement("div");
+    div.className = "booking-card";
+      
     div.innerHTML = `
+    
       <h3>Room: ${booking.roomId}</h3>
+      <p>customerName:${booking.name}</P>
       <p>Check-in: ${booking.checkin}</p>
       <p>Check-out: ${booking.checkout}</p>
       <p>Status: ${booking.status}</p>
@@ -25,5 +34,6 @@ auth.onAuthStateChanged(async (user) => {
 
     `;
     container.appendChild(div);
+    }
   });
 });
