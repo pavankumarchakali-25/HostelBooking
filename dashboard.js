25de9ea71ae6026f2bdc283f9797072b3c00eba7
pavankumarchakali-25 <c.pavan9949@gmail.com>
2025-09-14 16:34:14 +0530

    dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,7 +1,8 @@
-import { auth, db } from "./firebase-config.js";
+import { auth } from "./firebase-config.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import { collection, query, where, getDocs } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
-auth.onAuthStateChanged(async (user) => {
+onAuthStateChanged(auth, async (user) => {
   if (!user) {
     window.location.href = "login.html";
     return;
@@ -11,29 +12,26 @@ auth.onAuthStateChanged(async (user) => {
   const snapshot = await getDocs(q);
 
   const container = document.getElementById("booking-list");
+
+  if (snapshot.empty) {
+    container.innerHTML = "<p>No bookings found</p>";
+    return;
+  }
+
   snapshot.forEach((doc) => {
     const booking = doc.data();
-    if(booking==null) {
     const div = document.createElement("div");
     div.className = "booking-card";
-      div.innerHTML = ' <p>No Bookings</p>';
-        container.appendChild(div);
-    } else{
-      const div = document.createElement("div");
-    div.className = "booking-card";
-      
+
     div.innerHTML = `
-    
-      <h3>Room: ${booking.roomId}</h3>
-      <p>customerName:${booking.name}</P>
+      <h3>Room: ${booking.roomName}</h3>
+      <p>Customer: ${booking.userName}</p>
       <p>Check-in: ${booking.checkin}</p>
       <p>Check-out: ${booking.checkout}</p>
       <p>Status: ${booking.status}</p>
-      <p>Room: ${booking.roomName}</p>
       <p>Type: ${booking.roomType}</p>
-
     `;
+
     container.appendChild(div);
-    }
   });
 });
